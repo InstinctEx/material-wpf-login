8a7c01ef7ef50ecb8158186c156526fa9e15f20b
Instinct <26149719+InstinctEx@users.noreply.github.com>
2022-03-11 21:17:52 +0200

    Add files via upload

---
+++ Login/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paleteHelper = new PaletteHelper();
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paleteHelper.GetTheme();
+            if(IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                IsDarkTheme =true;
+                theme.SetBaseTheme(Theme.Dark);
+
+            }
+            paleteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        
+    }
+}
